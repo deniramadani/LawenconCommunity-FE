99d226cf1bc6bc663d927e2b126afb26e294f0b4
rezimaulana <rsazrm@gmail.com>
2022-12-05 17:58:14 +0700

    guard mainarea

--- projects/adminarea/src/app/pages/master/master.routing.ts
+++ projects/adminarea/src/app/pages/master/master.routing.ts
@@ -1,37 +1,36 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "projects/mainarea/src/app/auth/login/login.component";
-import { ContentAdminComponent } from "../../layout/content/admin/content.admin.component";
+import { CanActiveAuth } from "projects/mainarea/src/app/guard/can-active-auth.guard";
 
-const routes : Routes = [
+const routes: Routes = [
     {
         path: '',
-        component : LoginComponent
+        component: LoginComponent,
+        canActivate: [CanActiveAuth]
     },
-   
-    {
-        path : 'positions',
-        loadChildren : ()=> import('../master/position/position.module').then(u => u.PositionModule)
-    },
-    {
-        path : 'industries',
-        loadChildren : ()=> import('../master/industry/industry.module').then(u => u.IndustriesModule)
-    },
-    {
-        path : 'users',
-        loadChildren : ()=> import('../master/users/users.module').then(u => u.UsersModule)
-    },
-   
 
-
+    {
+        path: 'positions',
+        loadChildren: () => import('../master/position/position.module').then(u => u.PositionModule)
+    },
+    {
+        path: 'industries',
+        loadChildren: () => import('../master/industry/industry.module').then(u => u.IndustriesModule)
+    },
+    {
+        path: 'users',
+        loadChildren: () => import('../master/users/users.module').then(u => u.UsersModule)
+    },
 ]
 
 @NgModule({
-    imports : [
+    imports: [
         RouterModule.forChild(routes)
     ],
-    exports : [
+    exports: [
         RouterModule
     ]
 })
+
 export class MasterRouting { }
--- projects/mainarea/src/app/profile/profile.routing.ts
+++ projects/mainarea/src/app/profile/profile.routing.ts
@@ -2,6 +2,9 @@ import { NgModule } from "@angular/core"
 import { RouterModule, Routes } from "@angular/router"
 import { ContentAdminComponent } from "projects/adminarea/src/app/layout/content/admin/content.admin.component"
 import { ContentMemberComponent } from "projects/memberarea/src/app/layout/content/content.member.component"
+import { AdminGuard } from "../guard/admin.guard"
+import { MemberGuard } from "../guard/member.guard"
+import { SuperAdminGuard } from "../guard/super-admin.guard"
 import { ViewProfileComponent } from "./view-profile/view-profile.component"
 
 const routes : Routes = [
@@ -10,21 +13,24 @@ const routes : Routes = [
         component : ContentAdminComponent,
         children : [
             {  path : '', component : ViewProfileComponent},
-        ]
+        ],
+        canActivate : [AdminGuard]
     },
     {
         path : 'super-admin',
         component : ContentAdminComponent,
         children : [
             {  path : '', component : ViewProfileComponent},
-        ]
+        ],
+        canActivate : [SuperAdminGuard]
     },
     {
         path : 'members',
         component : ContentMemberComponent,
         children : [
             {  path : '', component : ViewProfileComponent},
-        ]
+        ],
+        canActivate : [MemberGuard]
     },
 ]
 @NgModule({
