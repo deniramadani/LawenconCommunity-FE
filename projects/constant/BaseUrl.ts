60fb9886b00a79cd22d59b15c433ba2a838276e6
fransisko88 <fransiskosihombing@gmail.com>
2022-12-09 16:48:52 +0700

    icons favicon app

--- projects/constant/BaseUrl.ts
+++ projects/constant/BaseUrl.ts
@@ -2,7 +2,7 @@ export enum BASE_URL{
   
   BASE_URL = 'http://localhost:8080',
   // WAR_URL = 'http://localhost:8080/community-0.0.1-SNAPSHOT',
-  // LAN_URL = 'http://192.168.10.169:8080',
+  // BASE_URL = 'http://192.168.10.169',
   // SERVER_URL='http://ec2-108-137-96-1.ap-southeast-3.compute.amazonaws.com:8080'
 
 }
